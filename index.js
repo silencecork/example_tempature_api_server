9cf866a30b49f620df0ef0d7212b380b917f4f94
Justin2Liu@taiwanmobile.com <Justin2Liu@taiwanmobile.com>
2015-02-20 21:42:10 +0800

    commit codes

---
+++ index.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var mongodb = require('mongodb');
+var moment = require('moment');
+
+var uri = 'mongodb://liu71101:2ilxldau@ds045011.mongolab.com:45011/sensor-statistic';
+
+mongodb.MongoClient.connect(uri, function(err, db) {
+	if (err) {
+		console.log('connect mongo db error ' + err);
+	} else {
+		console.log('connect mongo db success');
+		var app = express();
+
+		app.use(express.static(__dirname + '/public'));
+
+		app.get('/api/createDataPoint', function(request, response) {
+			if (!request.query.value) {
+				__sendErrorResponse(response, 403, 'No query parameters value');
+				return;
+			}
+
+			var title = request.query.value;
+			var timeMillis = moment();
+			var time = timeMillis.format('MM/DD hh:mm:ss');
+			var insert = {
+				_id: timeMillis.unix(),
+				value : title,
+				time : time
+			};
+			var items = db.collection('sensor_history');
+			items.insert(insert, function(err, result) {
+				if (err) {
+					__sendErrorResponse(response, 406, err);
+				} else {
+					response.status(200).send(result);
+					response.end();
+				}
+			});
+		});
+
+		app.get('/api/queryDataPoint', function(request, response) {
+			var items = db.collection('sensor_history');
+
+			var limit = parseInt(request.query.limit, 10) || 100;
+
+			items.find().sort({$natural: -1}).limit(limit).toArray(function (err, docs) {
+				if (err) {
+					console.log(err);
+					__sendErrorResponse(response, 406, err);
+				} else {
+					response.status(200).send(docs);
+					response.end();
+				}
+			});
+		});
+
+		app.listen(process.env.PORT || 5000);
+		console.log('port ' + (process.env.PORT || 5000));
+	}
+
+	function __sendErrorResponse(response, code, content) {
+		var ret = {
+			err: code,
+			desc : content 
+		};
+		response.status(code).send(ret);
+		response.end();
+	}
+});
+
+
